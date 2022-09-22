65edfcd03c7fa01c7a70f2e76d3021fb2a32a63f
Arkanheal <arkanheal@hotmail.fr>
2022-09-22 21:58:47 +0200

    feat(game): Adds check for victory function
    Kinda dirty, probably a way to do it cleaner

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -5,26 +5,69 @@ import { useGridStore } from '@/stores/grid.js'
 
 const store = useGridStore();
 
-function checkVictory(index: number, index_row: number) {
-
-  let currentSymbol = store.grid[index_row][index].symbol;
-
+function isValidTile(index_col : number, index : number){
+  return index_col < 4 && index_col >= 0 && index < 4 && index >= 0;
 }
 
-function checkTurn(index: number, index_row : number) {
+function checkVictory(index: number, index_col: number) {
 
-  if(!store.grid[index_row][index].clickable){
+  let currentSymbol : String = store.grid[index_col][index].symbol;
+  
+  // First surroundings
+  let rightSymbol : String = isValidTile(index_col + 1, index) ? store.grid[index_col + 1][index].symbol:"";
+  let leftSymbol : String = isValidTile(index_col - 1, index) ? store.grid[index_col - 1][index].symbol:"";
+  let botSymbol : String = isValidTile(index_col, index + 1) ? store.grid[index_col][index + 1].symbol:"";
+  let topSymbol : String = isValidTile(index_col, index - 1) ? store.grid[index_col][index - 1].symbol:"";
+  let topLeftSymbol : String = isValidTile(index_col - 1, index - 1) ? store.grid[index_col - 1][index - 1].symbol:"";
+  let topRightSymbol : String = isValidTile(index_col + 1, index - 1) ? store.grid[index_col + 1][index - 1].symbol:"";
+  let botLeftSymbol : String =  isValidTile(index_col - 1, index + 1) ? store.grid[index_col - 1][index + 1].symbol:"";  
+  let botRightSymbol : String = isValidTile(index_col + 1, index + 1) ? store.grid[index_col + 1][index + 1].symbol:"";
+  
+  if ((currentSymbol === rightSymbol && rightSymbol === leftSymbol) || 
+    (currentSymbol === topSymbol && topSymbol === botSymbol) ||
+    (currentSymbol === topLeftSymbol && topLeftSymbol === botRightSymbol) ||
+    (currentSymbol === topRightSymbol && topRightSymbol === botLeftSymbol)) {
+    console.log("Won");
+  }
+
+  // Second surroundings
+  let rightFarSymbol : String = isValidTile(index_col + 2, index) ? store.grid[index_col + 2][index].symbol:"";
+  let leftFarSymbol : String = isValidTile(index_col - 2, index) ? store.grid[index_col - 2][index].symbol:"";
+  let botFarSymbol : String = isValidTile(index_col, index + 2) ? store.grid[index_col][index + 2].symbol:"";
+  let topFarSymbol : String = isValidTile(index_col, index - 2) ? store.grid[index_col][index - 2].symbol:"";
+  let topLeftFarSymbol : String = isValidTile(index_col - 2, index - 2) ? store.grid[index_col - 2][index - 2].symbol:"";
+  let topRightFarSymbol : String = isValidTile(index_col + 2, index - 2) ? store.grid[index_col + 2][index - 2].symbol:"";
+  let botLeftFarSymbol : String =  isValidTile(index_col - 2, index + 2) ? store.grid[index_col - 2][index + 2].symbol:"";  
+  let botRightFarSymbol : String = isValidTile(index_col + 2, index + 2) ? store.grid[index_col + 2][index + 2].symbol:"";
+  
+  if ((currentSymbol === rightSymbol && rightSymbol === rightFarSymbol) || 
+    (currentSymbol === leftSymbol && leftSymbol === leftFarSymbol) ||
+    (currentSymbol === botSymbol && botSymbol === botFarSymbol) ||
+    (currentSymbol === topSymbol && topSymbol === topFarSymbol) ||
+    (currentSymbol === topLeftSymbol && topLeftSymbol === topLeftFarSymbol) ||
+    (currentSymbol === topRightSymbol && topRightSymbol === topRightFarSymbol) ||
+    (currentSymbol === botLeftSymbol && botLeftSymbol === botLeftFarSymbol) ||
+    (currentSymbol === botRightSymbol && botRightSymbol === botRightFarSymbol)) {
+    console.log("Won");
+  }
+
+  
+}
+
+function checkTurn(index: number, index_col : number) {
+
+  if(!store.grid[index_col][index].clickable){
     return
   }
 
   if(store.turn === 0){
-    store.grid[index_row][index].symbol = store.symbols[store.turn];
+    store.grid[index_col][index].symbol = store.symbols[store.turn];
   }
 
-  store.grid[index_row][index].symbol = store.symbols[store.turn%4];
-  store.grid[index_row][index].clickable = false;
+  store.grid[index_col][index].symbol = store.symbols[store.turn%4];
+  store.grid[index_col][index].clickable = false;
 
-  checkVictory(index, index_row);
+  checkVictory(index, index_col);
 
   store.turn++;
 
@@ -36,14 +79,14 @@ function checkTurn(index: number, index_row : number) {
 
 <div id="main_grid">
 
-  <div class="row" v-for="(row, index_row,) in store.grid"
-    :key="index_row"
+  <div class="col" v-for="(col, index_col,) in store.grid"
+    :key="index_col"
   >
   <Tile
-    v-for="(tile, index,) in row"
+    v-for="(tile, index,) in col"
     :key="index"
     :symbol="tile.symbol"
-    @checkTurn="checkTurn(index, index_row)"
+    @checkTurn="checkTurn(index, index_col)"
   /></div>
 
 </div>
