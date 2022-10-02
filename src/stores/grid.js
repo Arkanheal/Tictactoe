c50255e4366a3ede88247190d73d4bc564c8230d
Arkanheal <arkanheal@hotmail.fr>
2022-10-02 10:51:32 +0200

    fix(gridStore): Async/Await syntax

--- src/stores/grid.js
+++ src/stores/grid.js
@@ -1,12 +1,12 @@
 import { defineStore } from 'pinia';
-import { addDoc, collection } from 'firebase/firestore';
+import { addDoc, collection, getDocs } from 'firebase/firestore';
 import grid_json from '../assets/json/grid.json';
 
 export const useGridStore = defineStore('grid', () => {
 
-  function newGame(){
+  const addGame = async () => {
     const json = JSON.parse(JSON.stringify(grid_json));
-    try{
+    try {
       await addDoc(collection(db, "games"), {
         id: $cookies.get("gameId"),
         turn: 0,
@@ -19,9 +19,17 @@ export const useGridStore = defineStore('grid', () => {
     }
   }
 
-  const grids = await getDocs(collection(db, "games"));
+  const getGames = async () => {
+    await getDocs(collection(db, "games"));
+  }
+
+  function newGame(){
+    addGame();
+  }
+
+  const grids = getGames();
   const symbols = ["Cross","Triangle","Circle","Square"];
 
-  return { turn, grids, symbols, newGame };
+  return { grids, symbols, newGame };
 
 })
